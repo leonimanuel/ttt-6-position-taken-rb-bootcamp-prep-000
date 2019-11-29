aa87516accda0c0e0a48d9b05d1b2c05c2803ec1
Leon Malisov <leonmalisov@gmail.com>
2019-11-29 16:32:00 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if board[index] == " "
-    false
+    "blorp"
   # elsif board[index] == "X" || board[index] == "O"
   #   true
   end
